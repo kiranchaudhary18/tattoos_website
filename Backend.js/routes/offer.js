36b8e0217c6fd211c8bd92f60e271d596b8059a0
kiranchaudhary18 <kiran.chaudhary.cg@gmail.com>
2025-04-27 14:28:24 +0530

    add the offer backend files routes and models

---
+++ Backend.js/routes/offer.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const Offer = require("../models/Offer");
+
+router.get("/", async (req, res) => {
+    try {
+        const offers = await Offer.getOffers();  
+        console.log("Offers in DB:", offers); 
+        res.json(offers); 
+    } catch (err) {
+        console.error("❌ Error fetching offers:", err.message);
+        res.status(500).json({ error: err.message }); 
+    }
+});
+
+
+
+module.exports = router;
